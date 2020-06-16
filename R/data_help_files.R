cc91a3a3075a9acac0c168704caf84ff22bab7a7
Theo Roe <theo@jumpingrivers.com>
2020-06-16 15:53:29 +0100

    bumping version and adding food data

---
+++ R/data_help_files.R
@@ -0,0 +1,11 @@
+#' Food consumption figures for countries in Europe
+#'
+#' Taken from a larger data set of the world
+#'
+#'@name food
+#'@docType data
+#'@return A tibble or data frame.
+#'@keywords datasets
+#'@examples
+#' data(food)
+NULL
